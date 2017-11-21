3d6da43b6d03548f60057c76b5c99f2249030418
jl137275 <33095838+jl137275@users.noreply.github.com>
2017-11-21 10:30:44 -0600

    finished Roman Numeral Converter

---
+++ Roman Numeral Converter.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtDecimalInteger = New System.Windows.Forms.TextBox()
+        Me.lblRomanNumeral = New System.Windows.Forms.Label()
+        Me.btnConvert = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 34)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(201, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter a decimal integer in the range 1-10:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(35, 130)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(142, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Equivalent Roman Numeral: "
+        '
+        'txtDecimalInteger
+        '
+        Me.txtDecimalInteger.Location = New System.Drawing.Point(259, 34)
+        Me.txtDecimalInteger.Name = "txtDecimalInteger"
+        Me.txtDecimalInteger.Size = New System.Drawing.Size(100, 20)
+        Me.txtDecimalInteger.TabIndex = 2
+        '
+        'lblRomanNumeral
+        '
+        Me.lblRomanNumeral.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblRomanNumeral.Location = New System.Drawing.Point(256, 130)
+        Me.lblRomanNumeral.Name = "lblRomanNumeral"
+        Me.lblRomanNumeral.Size = New System.Drawing.Size(100, 23)
+        Me.lblRomanNumeral.TabIndex = 3
+        '
+        'btnConvert
+        '
+        Me.btnConvert.Location = New System.Drawing.Point(60, 215)
+        Me.btnConvert.Name = "btnConvert"
+        Me.btnConvert.Size = New System.Drawing.Size(75, 23)
+        Me.btnConvert.TabIndex = 4
+        Me.btnConvert.Text = "Convert"
+        Me.btnConvert.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(244, 215)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(408, 262)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnConvert)
+        Me.Controls.Add(Me.lblRomanNumeral)
+        Me.Controls.Add(Me.txtDecimalInteger)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Roman Numeral Converter"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtDecimalInteger As System.Windows.Forms.TextBox
+    Friend WithEvents lblRomanNumeral As System.Windows.Forms.Label
+    Friend WithEvents btnConvert As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ Roman Numeral Converter.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+
+    Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+        If txtDecimalInteger.Text = 1 Then
+            lblRomanNumeral.Text = "I"
+        ElseIf txtDecimalInteger.Text = 2 Then
+            lblRomanNumeral.Text = "II"
+        ElseIf txtDecimalInteger.Text = 3 Then
+            lblRomanNumeral.Text = "III"
+        ElseIf txtDecimalInteger.Text = 4 Then
+            lblRomanNumeral.Text = "IV"
+        ElseIf txtDecimalInteger.Text = 5 Then
+            lblRomanNumeral.Text = "V"
+        ElseIf txtDecimalInteger.Text = 6 Then
+            lblRomanNumeral.Text = "VI"
+        ElseIf txtDecimalInteger.Text = 7 Then
+            lblRomanNumeral.Text = "VII"
+        ElseIf txtDecimalInteger.Text = 8 Then
+            lblRomanNumeral.Text = "VIII"
+        ElseIf txtDecimalInteger.Text = 9 Then
+            lblRomanNumeral.Text = "IX"
+        ElseIf txtDecimalInteger.Text = 10 Then
+            lblRomanNumeral.Text = "X"
+        End If
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
