2de53c4356a0531b2e24b3310da65557b669cd74
jl137275 <33095838+jl137275@users.noreply.github.com>
2017-11-30 07:52:34 -0600

    finished age program

---
+++ Age Program/Age Program/Form1.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtInputAge = New System.Windows.Forms.TextBox()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.btnInsult = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(38, 69)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(56, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Input Age:"
+        '
+        'txtInputAge
+        '
+        Me.txtInputAge.Location = New System.Drawing.Point(100, 66)
+        Me.txtInputAge.Name = "txtInputAge"
+        Me.txtInputAge.Size = New System.Drawing.Size(100, 20)
+        Me.txtInputAge.TabIndex = 1
+        '
+        'lblResult
+        '
+        Me.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblResult.Location = New System.Drawing.Point(12, 124)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(237, 23)
+        Me.lblResult.TabIndex = 2
+        '
+        'btnInsult
+        '
+        Me.btnInsult.Location = New System.Drawing.Point(12, 227)
+        Me.btnInsult.Name = "btnInsult"
+        Me.btnInsult.Size = New System.Drawing.Size(75, 23)
+        Me.btnInsult.TabIndex = 3
+        Me.btnInsult.Text = "Insult"
+        Me.btnInsult.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(100, 227)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 4
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(197, 227)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnInsult)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.txtInputAge)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtInputAge As System.Windows.Forms.TextBox
+    Friend WithEvents lblResult As System.Windows.Forms.Label
+    Friend WithEvents btnInsult As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ Age Program/Age Program/Form1.vb
@@ -0,0 +1,40 @@
+﻿Public Class Form1
+
+
+
+    Private Sub btnInsult_Click(sender As Object, e As EventArgs) Handles btnInsult.Click
+        Dim IntAge As Integer
+        Try
+            If IntAge < 10 Then
+                lblResult.Text = "Can you even read?"
+            ElseIf IntAge >= 10 And IntAge < 14 Then
+                lblResult.Text = "There are no words to express how naive you are."
+            ElseIf IntAge >= 14 And IntAge < 18 Then
+                lblResult.Text = "You have no clue how the real world works."
+            ElseIf IntAge >= 18 And IntAge < 24 Then
+                lblResult.Text = "You won't make it."
+            ElseIf IntAge >= 24 And IntAge < 30 Then
+                lblResult.Text = "Try to keep a steady job."
+            ElseIf IntAge >= 30 And IntAge < 40 Then
+                lblResult.Text = "You're almost over the hill."
+            ElseIf IntAge >= 40 And IntAge < 60 Then
+                lblResult.Text = "You've made it over the hill"
+            ElseIf IntAge >= 60 Then
+                lblResult.Text = "Don't fall, you might need life alert."
+            End If
+        Catch ex As Exception
+            lblResult.Text = "Enter a numeric value."
+
+        End Try
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        lblResult.Text = ""
+        txtInputAge.Text = ""
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
